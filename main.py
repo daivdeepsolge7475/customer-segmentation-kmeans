e53ee6f60b6a8820dd954e1312dc5db2805eaa6e
Daivdeep Solge <activeboy7475@gmail.com>
2026-08-04 20:32:03 +0530

    project complete

--- main.py
+++ main.py
@@ -8,3 +8,151 @@ print(df.head())
 print(df.shape)
 print(df.columns)
 print(df.info())
+
+print("\n===== Missing Values =====")
+print(df.isnull().sum())
+
+print("\n===== Duplicate Rows =====")
+print(df.duplicated().sum())
+
+print("\n===== Statistical Summary =====")
+print(df.describe())
+
+print("\n===== Gender Counts =====")
+print(df["Gender"].value_counts())
+
+plt.figure(figsize=(5,5))
+
+df["Gender"].value_counts().plot(
+    kind="pie",
+    autopct="%1.1f%%",
+    startangle=90
+)
+
+plt.title("Gender Distribution")
+plt.ylabel("")
+plt.show()
+
+plt.figure(figsize=(8,5))
+
+plt.hist(
+    df["Age"],
+    bins=15,
+    edgecolor="black"
+)
+
+plt.title("Age Distribution")
+plt.xlabel("Age")
+plt.ylabel("Number of Customers")
+
+plt.show()
+
+plt.figure(figsize=(8,5))
+
+plt.hist(
+    df["Spending Score (1-100)"],
+    bins=15,
+    edgecolor="black"
+)
+
+plt.title("Spending Score Distribution")
+plt.xlabel("Spending Score")
+plt.ylabel("Frequency")
+
+plt.show()
+
+plt.figure(figsize=(8,6))
+
+plt.scatter(
+    df["Annual Income (k$)"],
+    df["Spending Score (1-100)"]
+)
+
+plt.title("Annual Income vs Spending Score")
+plt.xlabel("Annual Income (k$)")
+plt.ylabel("Spending Score (1-100)")
+
+plt.show()
+
+plt.figure(figsize=(8,6))
+
+plt.scatter(
+    df["Age"],
+    df["Spending Score (1-100)"]
+)
+
+plt.title("Age vs Spending Score")
+plt.xlabel("Age")
+plt.ylabel("Spending Score")
+
+plt.show()
+
+from sklearn.cluster import KMeans
+
+X = df[["Annual Income (k$)", "Spending Score (1-100)"]]
+
+wcss = []
+
+for i in range(1, 11):
+    kmeans = KMeans(
+        n_clusters=i,
+        init="k-means++",
+        random_state=42,
+        n_init=10
+    )
+
+    kmeans.fit(X)
+    wcss.append(kmeans.inertia_)
+
+    plt.figure(figsize=(8,5))
+
+plt.plot(range(1,11), wcss, marker="o")
+
+plt.title("Elbow Method")
+plt.xlabel("Number of Clusters (K)")
+plt.ylabel("WCSS")
+
+plt.grid(True)
+
+plt.show()
+
+print("\n===== Applying K-Means Clustering =====")
+
+kmeans = KMeans(
+    n_clusters=5,
+    init="k-means++",
+    random_state=42,
+    n_init=10
+)
+
+df["Cluster"] = kmeans.fit_predict(X)
+
+print(df[["Annual Income (k$)", "Spending Score (1-100)", "Cluster"]].head(10))
+
+plt.figure(figsize=(8,6))
+
+plt.scatter(
+    df["Annual Income (k$)"],
+    df["Spending Score (1-100)"],
+    c=df["Cluster"],
+    cmap="viridis",
+    s=80
+)
+
+plt.scatter(
+    kmeans.cluster_centers_[:,0],
+    kmeans.cluster_centers_[:,1],
+    c="red",
+    marker="X",
+    s=300,
+    label="Centroids"
+)
+
+plt.title("Customer Segments using K-Means")
+plt.xlabel("Annual Income (k$)")
+plt.ylabel("Spending Score (1-100)")
+plt.legend()
+
+plt.show()
+
+
